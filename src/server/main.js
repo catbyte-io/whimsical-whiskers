500dbe5d8900b889a2543108356a76cf9468c06a
catbyte-io <catbyte.io@gmail.com>
2025-02-02 19:42:44 -0700

    Configure to use nunjucks templating and specify static directory. Add homepage route for index.njk

--- src/server/main.js
+++ src/server/main.js
@@ -1,10 +1,26 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import nunjucks from 'nunjucks';
 
 const app = express();
+
+// Configure app to use nunjucks for templating
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});
+
+app.use(express.static('public'));
 
 const server = app.listen(3000, "0.0.0.0", () =>
   console.log("Server is listening...")
 );
 
 ViteExpress.bind(app, server);
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('index.njk', 
+    { characters },
+  );
+});
