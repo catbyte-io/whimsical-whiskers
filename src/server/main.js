e4d565d5520cae4f78031cbbb319ac0c664c027f
catbyte-io <catbyte.io@gmail.com>
2025-02-08 21:18:05 -0700

    Configure static source

--- src/server/main.js
+++ src/server/main.js
@@ -13,6 +13,7 @@ nunjucks.configure('views', {
 });
 
 app.use(express.static('public'));
+app.use(express.static('src/client'));
 
 const server = app.listen(3000, "0.0.0.0", () =>
   console.log("Server is listening...")
