32473a82b08263d644ba8a2c70267d8b2881e6d9
catbyte-io <catbyte.io@gmail.com>
2025-03-06 20:46:16 -0700

    Move the animal profile logic to a separate function that can be called when nav arrows are clicked

--- src/client/find.js
+++ src/client/find.js
@@ -38,6 +38,27 @@ async function fetchPets(zipcode) {
     }
 }
 
+// Update the animal profile with new data
+function updateAnimalProfile(animal) {
+    // Clear the profile to start fresh
+    animalProfile.innerHTML = '';
+
+    // Add animal profile photo
+    const photoUrls = animal.photos;
+    if (photoUrls && photoUrls.length > 0) {
+        let animalPhoto = document.createElement('img');
+        animalPhoto.src = photoUrls[0].large;
+        animalPhoto.className = 'profile-photo';
+        animalProfile.appendChild(animalPhoto);
+    }
+
+    // Add animal name
+    const name = animal.name;
+    let animalName = document.createElement('h3');
+    animalName.innerHTML = `${name}`;
+    animalProfile.appendChild(animalName);
+};
+
 // Set up the button to find pets
 function setupFind(element) {
     // Button text
@@ -51,6 +72,8 @@ function setupFind(element) {
         // Clear data from animalProfile to start fresh
         animalProfile.innerHTML = '';
 
+        // Set variables to start new
+        var counter = 0;
         var animalData;
 
         // Obtain the input value for the zipcode
@@ -61,22 +84,57 @@ function setupFind(element) {
             animalData = await fetchPets(zipCode);
             console.log(animalData);
 
+            // Get the number of animals found
+            const numAnimals = animalData.animals.length;
+
             // Display animal data
-            if (animalData && animalData.animals && animalData.animals.length > 0) {
-                const animal = animalData.animals[0];
-                console.log(animal);
-                const photoUrls = animal.photos;
-                if (photoUrls && photoUrls.length > 0) {
-                    let animalPhoto = document.createElement('img');
-                    animalPhoto.src = photoUrls[0].small;
-                    animalPhoto.className = 'profile-photo';
-                    animalProfile.appendChild(animalPhoto);
-                }
+            if (animalData && animalData.animals && numAnimals > 0) {
+                // Create navigation buttons
+                let prevButton = document.createElement('button');
+                let nextButton = document.createElement('button');
+
+                prevButton.className = 'icon';
+                nextButton.className = 'icon';
+
+                let prevIcon = document.createElement('i');
+                let nextIcon = document.createElement('i');
+
+                prevIcon.className = 'bi bi-caret-left-fill';
+                nextIcon.className = 'bi bi-caret-right-fill';
+
+                prevButton.appendChild(prevIcon);
+                nextButton.appendChild(nextIcon);
+
+                // Add nav buttons to page
+                prevPage = document.getElementById('prevPage');
+                nextPage = document.getElementById('nextPage');
+
+                prevPage.appendChild(prevButton);
+                nextPage.appendChild(nextButton);
+
+                // Adapted from the default Vite file 'counter.js'
+
+                const setCounter = (count) => {
+                  counter = count;
+                };
+
+                /* Listen for the navigation arrows to be clicked to display the next animal profile, 
+                 using the value of counter to access different ones*/
+                prevButton.addEventListener("click", () => {
+                    setCounter((counter - 1 + numAnimals) % numAnimals);
+                    updateAnimalProfile(animalData.animals[counter]);
+                });
+                nextButton.addEventListener("click", () => {
+                    setCounter((counter + 1) % numAnimals);
+                    updateAnimalProfile(animalData.animals[counter]);
+                });
+                
+                // Display the first animal
+                updateAnimalProfile(animalData.animals[counter]);
             }
         }
     });
 }
-
 
 
 export { setupFind }
